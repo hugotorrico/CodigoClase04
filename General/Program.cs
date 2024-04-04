08bba7552916a7dfce0d4ed78d8b7b391e6dbde7
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2024-04-03 22:21:21 -0500

    Ejemplo Final Funciones y Procedimientos

---
+++ General/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//Variable global
+string nombreEmpresa = "Codigo Tecsup";
+double IGV = 18;
+double Descuento4TACategoria = 8;
+
+double CalcularSueldoNeto(double sueldo)
+{
+    return sueldo - (sueldo * Descuento4TACategoria)/100;
+}
+
+//Conjunto de instrucciones
+void MostarResultados(double sueldo,string Nombres)
+{
+    Console.WriteLine("La empresa:");
+    Console.WriteLine(nombreEmpresa);
+    Console.WriteLine("El trabajador:");
+    Console.WriteLine(Nombres);
+    Console.WriteLine("El sueldo Neto:");
+    Console.WriteLine(CalcularSueldoNeto(sueldo));
+  
+}
+
+MostarResultados(5000, "Hugo Torrico");
+
+
+Console.Read();
+
---
+++ Procedimientos/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+
+//Procedimmiento
+void CalcularAreaTriangulo(int base1, int altura1)
+{
+    int resultado = (base1 * altura1) / 2;
+    Console.WriteLine("El area del triángulo:");
+    Console.WriteLine(resultado);
+}
+string NombresCompletos;
+void ConcatenarNombres(string Nombres, string Apellidos)
+{
+    NombresCompletos=Nombres + " " + Apellidos;
+}
+
+
+/*Ingresar el Sueldo y van a mostar el descuento
+ Si <1500 No hay descuento
+ Si >=1500 y <5000 => 8%
+ Si >=5000 y <10000 => 14%
+  Si >=10000 => 30%
+ */
+
+
+
+CalcularAreaTriangulo(10, 20);
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
